46830949deaebea77b38a5dde7bc5a14e987dbf9
xiaoweipeng <54565603+xiaoweipeng@users.noreply.github.com>
2021-10-14 15:12:34 +0800

    修复为form文件的内容支持tab和row

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,10 +1,18 @@
 {!! $start !!}
 <div class="box-body fields-group">
+    <div class="box-body fields-group pl-0 pr-0 pt-1" style="padding: 0 0 .5rem">
+        @if(! $tabObj->isEmpty())
+            @include('admin::form.tab', compact('tabObj'))
 
-    @foreach($fields as $field)
-        {!! $field->render() !!}
-    @endforeach
-
+            @foreach($fields as $field)
+                @if($field instanceof \Dcat\Admin\Form\Field\Hidden)
+                    {!! $field->render() !!}
+                @endif
+            @endforeach
+        @else
+            @include('admin::form.fields')
+        @endif
+    </div>
 </div>
 
 {!! $end !!}
